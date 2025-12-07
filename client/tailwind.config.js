df8f60fd07fee3f777991a7b17aa0d6cb9092455
Spark16x <spark2009971@gmail.com>
2025-12-07 15:49:52 +0530

    update

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default  {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+    "./app/**/*.{ts,tsx,js,jsx}",
+  ],
+ 
+  plugins: [require("tailwindcss-animate")],
+};
